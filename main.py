ec4c4b2a195fbdcd4cef57e41aa6cad76bd11176
luisa23-gut <lobogutierrezmarialuisa@gmail.com>
2024-12-03 18:47:59 -0400

    try e excepet foram adcionados no arquivo main na parte de cadastrar usuário, a partir da linha 37.

--- main.py
+++ main.py
@@ -33,52 +33,62 @@ def criar_usuario(usuarios):
     tipo_usuario = input('Escolha o tipo de usuário: ')
 
     nome = input("\nNome: ")
-    cpf = input("CPF: ")
-    rg = input("RG: ")
-    naturalidade = input("Naturalidade: ")
-    email = input("E-mail: ")
-    senha = input("Senha: ")
-    telefone = input("Número de celular: ")
-
-    if tipo_usuario == "1":
-        matricula = input("Matrícula: ")
-        pai = input("Pai: ")
-        mae = input("Mãe: ")
-        curso = input("Curso: ")
-        turma_turno = input("Turma/Turno: ")
-        serie = input("Série: ")
-        aluno = Aluno(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, pai, mae, curso, turma_turno, serie)
-        usuarios.append(aluno)
-        aluno.cadastro()
-
-    elif tipo_usuario == "2":
-        #verifica se há alunos cadastrados
-        if not any(isinstance(usuario, Aluno) for usuario in usuarios):
-            print("\nNão existem alunos cadastrados no sistema. Não é possível cadastrar um responsável.")
-            return None
-            
-        matricula = input("Matrícula do aluno: ")
-        chave_acesso = input("Chave de acesso fornecida pelo aluno: ")
-
-        #verifica se a chave de acesso e a matrícula correspondem a algum objeto
-        aluno_cadastrado = None
-        for usuario in usuarios:
-            if isinstance(usuario, Aluno) and usuario.get_matricula() == matricula and usuario.get_chave_acesso() == chave_acesso:
-                aluno_cadastrado = usuario
+    while True:
+        try: 
+            cpf = input("CPF: ")
+            rg = input("RG: ")
+            break
+        except ValueError:
+            print('Aceitamos apenas números. Digite novamente:')
+        naturalidade = input("Naturalidade: ")
+        email = input("E-mail: ")
+        senha = input("Senha: ")
+        while True:
+            try: 
+                telefone = input("Número de celular: ")
                 break
+            except ValueError:
+                print('Aceitamos apenas números. Digite novamente:')
 
-        if aluno_cadastrado:
-            responsavel = Responsavel(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, chave_acesso)
-            usuarios.append(responsavel)
-            responsavel.cadastro()
-            return responsavel
+        if tipo_usuario == "1":
+            matricula = input("Matrícula: ")
+            pai = input("Pai: ")
+            mae = input("Mãe: ")
+            curso = input("Curso: ")
+            turma_turno = input("Turma/Turno: ")
+            serie = input("Série: ")
+            aluno = Aluno(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, pai, mae, curso, turma_turno, serie)
+            usuarios.append(aluno)
+            aluno.cadastro()
+
+        elif tipo_usuario == "2":
+            #verifica se há alunos cadastrados
+            if not any(isinstance(usuario, Aluno) for usuario in usuarios):
+                print("\nNão existem alunos cadastrados no sistema. Não é possível cadastrar um responsável.")
+                return None
+                
+            matricula = input("Matrícula do aluno: ")
+            chave_acesso = input("Chave de acesso fornecida pelo aluno: ")
+
+            #verifica se a chave de acesso e a matrícula correspondem a algum objeto
+            aluno_cadastrado = None
+            for usuario in usuarios:
+                if isinstance(usuario, Aluno) and usuario.get_matricula() == matricula and usuario.get_chave_acesso() == chave_acesso:
+                    aluno_cadastrado = usuario
+                    break
+
+            if aluno_cadastrado:
+                responsavel = Responsavel(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, chave_acesso)
+                usuarios.append(responsavel)
+                responsavel.cadastro()
+                return responsavel
+            else:
+                print("\nChave de acesso ou matrícula inválida.")
+                return None
+
         else:
-            print("\nChave de acesso ou matrícula inválida.")
+            print("\nTipo de usuário inválido.")
             return None
-
-    else:
-        print("\nTipo de usuário inválido.")
-        return None
 
 
 def set_login(usuarios):
