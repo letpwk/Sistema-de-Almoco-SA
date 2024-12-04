55e52b29a8f4d4f2abd1804c26893b49140e3a9e
Beatriz Rocha <beatrizszr08@gmail.com>
2024-12-04 03:18:39 +0000

    try e except adicionados na biblioteca Cronograma

--- Cronograma.py
+++ Cronograma.py
@@ -44,52 +44,52 @@ class Cronograma:
             if troca == '1':  
                 self.consultarCronograma() 
                 while True:
-                    try:
-                        diaAdicionado = int(input('Selecione o dia da semana que deseja adicionar:\n1 - Segunda-feira\n2 - Terça-feira\n3 - Quarta-feira\n4 - Quinta-feira\n5 - Sexta-feira\nDigite aqui: '))
-                        if diaAdicionado == 1:
-                            print('Segunda-feira adicionado!')
-                            break
-                        elif diaAdicionado == 2:
-                            print('Terça-feira adicionado!')
-                            break
-                        elif diaAdicionado == 3:
-                            print('Quarta-feira adicionado!')
-                            break
-                        elif diaAdicionado == 4:
-                            print('Quinta-feira adicionado!')
-                            break
-                        elif diaAdicionado == 5:
-                            print('Sexta-feira adicionado!')
-                            break
-                        else:
-                            print('Número inválido! Por favor, digite novamente:')
-                    except ValueError:
-                        print('Número inválido! Por favor, digite novamente:')
+                        try:
+                            diaAdicionado = int(input('Selecione o dia da semana que deseja adicionar:\n1 - Segunda-feira\n2 - Terça-feira\n3 - Quarta-feira\n4 - Quinta-feira\n5 - Sexta-feira\nDigite aqui: '))
+                            if diaAdicionado == 1:
+                                print('Segunda-feira adicionado!')
+                                break
+                            elif diaAdicionado == 2:
+                                print('Terça-feira adicionado!')
+                                break
+                            elif diaAdicionado == 3:
+                                print('Quarta-feira adicionado!')
+                                break
+                            elif diaAdicionado == 4:
+                                print('Quinta-feira adicionado!')
+                                break
+                            elif diaAdicionado == 5:
+                                print('Sexta-feira adicionado!')
+                                break
+                            else:
+                                print('Número inválido! Por favor, digite novamente:')
+                        except ValueError:
+                                print('Número inválido! Por favor, digite novamente: ')
 
                 while True:
-                    try:
-                        diaRemovido = int(input('Selecione o dia da semana que deseja remover:\n1 - Segunda-feira\n2 - Terça-feira\n3 - Quarta-feira\n4 - Quinta-feira\n5 - Sexta-feira\nDigite aqui: '))
-                        if diaRemovido == 1:
-                            print('Segunda-feira removido!')
-                            break
-                        elif diaRemovido == 2:
-                            print('Terça-feira removido!')
-                            break
-                        elif diaRemovido == 3:
-                            print('Quarta-feira removido!')
-                            break
-                        elif diaRemovido == 4:
-                            print('Quinta-feira removido!')
-                            break
-                        elif diaRemovido == 5:
-                            print('Sexta-feira removido!')
-                            break
-                        else:
-                            print('Número inválido! Por favor, digite novamente:')
-                    except ValueError:
-                        print('Número inválido! Por favor, digite novamente:')
-                
-                break 
+                        try:
+                            diaRemovido = int(input('Selecione o dia da semana que deseja remover:\n1 - Segunda-feira\n2 - Terça-feira\n3 - Quarta-feira\n4 - Quinta-feira\n5 - Sexta-feira\nDigite aqui: '))
+                            if diaRemovido == 1:
+                                print('Segunda-feira removido!')
+                                break
+                            elif diaRemovido == 2:
+                                print('Terça-feira removido!')
+                                break
+                            elif diaRemovido == 3:
+                                print('Quarta-feira removido!')
+                                break
+                            elif diaRemovido == 4:
+                                print('Quinta-feira removido!')
+                                break
+                            elif diaRemovido == 5:
+                                print('Sexta-feira removido!')
+                                break
+                            else:
+                                print('Número inválido! Por favor, digite novamente:')
+                        except ValueError:
+                            print('Número inválido! Por favor, digite novamente: ')
+                    
+                        break 
 
             elif troca == '2': 
                 print('Você optou por não alterar o dia de almoço.')
