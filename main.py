13bccc074d43a1a58208d5a439992adb4d406589
Beatriz Rocha <beatrizszr08@gmail.com>
2024-12-05 02:54:23 +0000

    outro try e except adicionado na biblioteca main

--- main.py
+++ main.py
@@ -27,58 +27,59 @@ def find_aluno_by_matricula(usuarios, matricula):
     return None  # Retorna None caso não encontre nenhum aluno com a matrícula informada
 
 def criar_usuario(usuarios):
-    print('\nComo você deseja se cadastrar?')
-    print('1 - Aluno')
-    print('2 - Responsável')
-    tipo_usuario = input('Escolha o tipo de usuário: ')
+    try:
+        print('\nComo você deseja se cadastrar?')
+        print('1 - Aluno')
+        print('2 - Responsável')
+        tipo_usuario = input('Escolha o tipo de usuário: ')
 
-    nome = input("\nNome: ")
-    cpf = input("CPF: ")
-    rg = input("RG: ")
-    naturalidade = input("Naturalidade: ")
-    email = input("E-mail: ")
-    senha = input("Senha: ")
-    telefone = input("Número de celular: ")
+        nome = input("\nNome: ")
+        cpf = input("CPF: ")
+        rg = input("RG: ")
+        naturalidade = input("Naturalidade: ")
+        email = input("E-mail: ")
+        senha = input("Senha: ")
+        telefone = input("Número de celular: ")
 
-    if tipo_usuario == "1":
-        matricula = input("Matrícula: ")
-        pai = input("Pai: ")
-        mae = input("Mãe: ")
-        curso = input("Curso: ")
-        turma_turno = input("Turma/Turno: ")
-        serie = input("Série: ")
-        aluno = Aluno(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, pai, mae, curso, turma_turno, serie)
-        usuarios.append(aluno)
-        aluno.cadastro()
+        if tipo_usuario == "1":
+            matricula = input("Matrícula: ")
+            pai = input("Pai: ")
+            mae = input("Mãe: ")
+            curso = input("Curso: ")
+            turma_turno = input("Turma/Turno: ")
+            serie = input("Série: ")
+            aluno = Aluno(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, pai, mae, curso, turma_turno, serie)
+            usuarios.append(aluno)
+            aluno.cadastro()
 
-    elif tipo_usuario == "2":
-        #verifica se há alunos cadastrados
-        if not any(isinstance(usuario, Aluno) for usuario in usuarios):
-            print("\nNão existem alunos cadastrados no sistema. Não é possível cadastrar um responsável.")
-            return None
-            
-        matricula = input("Matrícula do aluno: ")
-        chave_acesso = input("Chave de acesso fornecida pelo aluno: ")
+        elif tipo_usuario == "2":
+            #verifica se há alunos cadastrados
+            if not any(isinstance(usuario, Aluno) for usuario in usuarios):
+                print("\nNão existem alunos cadastrados no sistema. Não é possível cadastrar um responsável.")
+                return None
+                
+            matricula = input("Matrícula do aluno: ")
+            chave_acesso = input("Chave de acesso fornecida pelo aluno: ")
 
-        #verifica se a chave de acesso e a matrícula correspondem a algum objeto
-        aluno_cadastrado = None
-        for usuario in usuarios:
-            if isinstance(usuario, Aluno) and usuario.get_matricula() == matricula and usuario.get_chave_acesso() == chave_acesso:
-                aluno_cadastrado = usuario
-                break
+            #verifica se a chave de acesso e a matrícula correspondem a algum objeto
+            aluno_cadastrado = None
+            for usuario in usuarios:
+                if isinstance(usuario, Aluno) and usuario.get_matricula() == matricula and usuario.get_chave_acesso() == chave_acesso:
+                    aluno_cadastrado = usuario
+                    break
 
-        if aluno_cadastrado:
-            responsavel = Responsavel(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, chave_acesso)
-            usuarios.append(responsavel)
-            responsavel.cadastro()
-            return responsavel
-        else:
-            print("\nChave de acesso ou matrícula inválida.")
-            return None
+            if aluno_cadastrado:
+                responsavel = Responsavel(nome, cpf, rg, naturalidade, email, senha, telefone, matricula, chave_acesso)
+                usuarios.append(responsavel)
+                responsavel.cadastro()
+                return responsavel
+            else:
+                print("\nChave de acesso ou matrícula inválida.")
+                return None
 
-    else:
-        print("\nTipo de usuário inválido.")
-        return None
+    except ValueError:
+            print("\nTipo de usuário inválido.")
+    return None
 
 
 def set_login(usuarios):
@@ -122,14 +123,14 @@ def menu(usuarios):
         break
 
 def consultar_cronograma(usuarios):
-    aluno = set_login(usuarios)  # Obtém o aluno logado
+    aluno = set_login(usuarios)  #obtém o aluno logado
     if isinstance(aluno, Aluno):
         print(f"Seu cronograma de almoço é: {aluno.consultar_cronograma()}")
     else:
         print("Você não tem permissão para consultar o cronograma de almoço.")
 
 def solicitar_troca(usuarios):
-    aluno = set_login(usuarios)  # Obtém o aluno logado
+    aluno = set_login(usuarios)  #obtém o aluno logado
     if isinstance(aluno, Aluno):
         dia_novo = input("Digite o dia novo para a troca (Ex: Segunda-feira): ")
         aluno.solicitar_troca(dia_novo)
